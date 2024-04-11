6c02896340dc0c1b49f4d741c5720eaab1971977
Garrett Ballreich <garrett.ballreich101@gmail.com>
2024-04-11 13:29:06 -0400

    added controler

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//instantiate F3 base
+$f3 = Base::instance();
+
+//define default root
+$f3->route('GET /', function() {
+
+    echo '<h1>Pet Home</h1>';
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run();
